4e3c1baa0ffd3e7abc60bf36e956c7f123705e5f
souz-dev <souz.developer@gmail.com>
2024-11-07 14:42:44 -0300

    feature/SingUpUseCase: add hashedPassword

--- src/application/useCases/SignUpUseCase.ts
+++ src/application/useCases/SignUpUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs";
 import { AccountAlreadyExists } from "../errors/accountAlreadyExists";
 import { prismaClient } from "../libs/prismaClient";
 
@@ -20,11 +21,13 @@ export class SignUpUseCase {
       throw new AccountAlreadyExists();
     }
 
+    const hashedPassword = await hash(password, 10);
+
     await prismaClient.account.create({
       data: {
         email,
         name,
-        password,
+        password: hashedPassword,
       },
     });
   }
